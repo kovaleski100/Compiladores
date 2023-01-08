4b208b68db06faa6b7a2fe277fcc66cf4a121a94
kovaleski100 <mfkovaleski.inf.ufrgs.br>
2023-01-08 22:38:04 +0100

    begin etapa 2

---
+++ etapa2/main.c
@@ -0,0 +1,22 @@
+#/********************************************************************** 
+# * INF01147 - Compiladores - Turma B (2022/2)                         *
+# *                                                                    *
+# * Felipe Bastos                                                      *
+# * Matheus Kovaleski                                      					 	 *
+# **********************************************************************/
+
+
+/*
+Programa principal de impressão de tokens.
+Este arquivo será posteriormente substituído.
+*/
+#include <stdio.h>
+#include "parser.tab.h" //arquivo gerado com bison -d parser.y
+extern int yylex_destroy(void);
+
+int main (int argc, char **argv)
+{
+  int ret = yyparse();
+  yylex_destroy();
+  return ret;
+}
