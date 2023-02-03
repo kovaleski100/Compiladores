9fcdb68e1b6db665b2063a5e3b3c24da515692ab
kovaleski100 <mfkovaleski.inf.ufrgs.br>
2023-02-03 21:08:51 +0100

    implementação das funções

--- etapa3/ast.c
+++ etapa3/ast.c
@@ -1,3 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "ast.h"
+
+ast* create_node(int tipo, valor_lexico valor)
+{
+    ast *node = (ast*)malloc(sizeof(ast));
+
+    node->filho = NULL;
+    node->tipo_token = tipo;
+    node->valor_lexico = valor;
+
+    return node;
+
+}
+
+// ast* create_leaf(int tipo, valor_lexico valor); precisa?
+ast* add_child(ast *arvore, ast nodo)
+{
+    realloc(arvore->filho, sizeof(arvore->filho)*(arvore->num_filhos+1));
+    arvore->filho[arvore->num_filhos] = nodo;
+    arvore->num_filhos += 1;
+    return arvore;
+}
+
+print_arvore(ast *arvore)
+{
+    for(int i=0; i< arvore->num_filhos; i++)
+    {
+        print_dados(&arvore->valor_lexico)
+    }
+    for(int i = 0; i< arvore->num_filhos; i++)
+    {
+        print_arvore(&arvore->filho[i]);
+    }
+}
+
+
+//acho que essa função tem que ser implementada conforme for ocorrendo os testes
+print_dados(ast *arvore)
+{
+
+}
--- etapa3/ast.h
+++ etapa3/ast.h
@@ -1,30 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-
-struct Literal
+union Literal
 {
     int d;
     float f;
-    char* s;
+    char *s;
     bool b;
 };
 
-struct valor_lexico
+typedef struct valor_lexico
 {
     int numero_linha;
-    char* valor_token;
+    char *valor_token;
     union Literal literal;
     /* data */
-};
+} valor_lexico;
 
-struct ast
+typedef struct ast
 {
     valor_lexico valor_lexico;
     int tipo_token;
     struct ast *filho;
+    int num_filhos;
     /* data */
-};
+} ast;
 
 ast* create_node(int tipo, valor_lexico valor);
 ast* create_leaf(int tipo, valor_lexico valor);
-ast* add_child(ast arvore, ast nodo);
-print_arvore(ast arvore);
+ast* add_child(ast *arvore, ast nodo);
+print_arvore(ast *arvore);
+print_dados(ast *arvore);
